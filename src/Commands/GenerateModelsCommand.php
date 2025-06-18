52b2ad5dc1e74a3f449657f98128914dea3c61fb
ridtichai.borseen <ridtichai.borseen@gmail.com>
2025-06-18 13:56:25 +0700

    18/06/2025

--- src/Commands/GenerateModelsCommand.php
+++ src/Commands/GenerateModelsCommand.php
@@ -1,10 +1,11 @@
 <?php
-// src/Commands/GenerateModelsCommand.php
+
 namespace ModelGenerator\Commands;
 
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\File;
+use Illuminate\Support\Str;
 
 class GenerateModelsCommand extends Command
 {
@@ -15,14 +16,29 @@ class GenerateModelsCommand extends Command
     {
         $database = config('database.connections.' . config('database.default') . '.database');
         $tables = $this->option('table') ? [$this->option('table')] : DB::select('SHOW TABLES');
-
         $tableKey = 'Tables_in_' . $database;
+
+        // Load foreign keys for relation detection
+        $foreignKeys = DB::select("
+            SELECT
+                TABLE_NAME,
+                COLUMN_NAME,
+                REFERENCED_TABLE_NAME,
+                REFERENCED_COLUMN_NAME
+            FROM
+                information_schema.KEY_COLUMN_USAGE
+            WHERE
+                TABLE_SCHEMA = ? AND
+                REFERENCED_TABLE_NAME IS NOT NULL
+        ", [$database]);
+
+        $relationshipMap = collect($foreignKeys)->groupBy('TABLE_NAME');
 
         foreach ($tables as $tableObj) {
             $table = $this->option('table') ? $tableObj : $tableObj->$tableKey;
-            $columns = DB::select("SHOW COLUMNS FROM $table");
+            $columns = DB::select("SHOW COLUMNS FROM `$table`");
 
-            $modelName = ucfirst(\Str::camel(\Str::singular($table)));
+            $modelName = ucfirst(Str::camel(Str::singular($table)));
             $path = app_path("Models/$modelName.php");
 
             if (File::exists($path) && !$this->option('force')) {
@@ -30,16 +46,54 @@ class GenerateModelsCommand extends Command
                 continue;
             }
 
+            // Fillable columns
             $fillable = collect($columns)
                 ->pluck('Field')
                 ->reject(fn($col) => in_array($col, ['id', 'created_at', 'updated_at']))
                 ->map(fn($col) => "'$col'")
                 ->implode(', ');
 
+            // BelongsTo relationships
+            $relations = '';
+            if ($relationshipMap->has($table)) {
+                foreach ($relationshipMap[$table] as $relation) {
+                    $relatedModel = ucfirst(Str::camel(Str::singular($relation->REFERENCED_TABLE_NAME)));
+                    $methodName = Str::camel(Str::singular($relation->REFERENCED_TABLE_NAME));
+
+                    $relations .= <<<EOD
+
+    public function {$methodName}()
+    {
+        return \$this->belongsTo(\\App\\Models\\{$relatedModel}::class, '{$relation->COLUMN_NAME}');
+    }
+
+EOD;
+                }
+            }
+
+            // HasMany relationships
+            $hasManyList = collect($foreignKeys)
+                ->filter(fn($fk) => $fk->REFERENCED_TABLE_NAME === $table);
+
+            foreach ($hasManyList as $fk) {
+                $relatedModel = ucfirst(Str::camel(Str::singular($fk->TABLE_NAME)));
+                $methodName = Str::camel(Str::plural($fk->TABLE_NAME));
+
+                $relations .= <<<EOD
+
+    public function {$methodName}()
+    {
+        return \$this->hasMany(\\App\\Models\\{$relatedModel}::class, '{$fk->COLUMN_NAME}');
+    }
+
+EOD;
+            }
+
+            // Read and inject into stub
             $stub = File::get(__DIR__ . '/../../stubs/model.stub');
             $code = str_replace(
-                ['{{ class }}', '{{ table }}', '{{ fillable }}'],
-                [$modelName, $table, $fillable],
+                ['{{ class }}', '{{ table }}', '{{ fillable }}', '{{ relations }}'],
+                [$modelName, $table, $fillable, $relations],
                 $stub
             );
 
